d6be31892f2f784b23a433035b5b850a73d3eea5
强淑婷 <863139668@qq.com>
2019-01-08 15:49:28 +0800

    test2shangchuan

--- gittest/gittest/ViewController.m
+++ gittest/gittest/ViewController.m
@@ -16,6 +16,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
