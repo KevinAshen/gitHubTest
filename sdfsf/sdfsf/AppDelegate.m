3d4da7fe727db77bb1d8cd5a05c59687ea90d335
jiangkaiwen <894912881@qq.com>
2019-01-10 22:08:55 +0800

    two

--- sdfsf/sdfsf/AppDelegate.m
+++ sdfsf/sdfsf/AppDelegate.m
@@ -7,7 +7,7 @@
 //
 
 #import "AppDelegate.h"
-
+#import "OneViewController.h"
 @interface AppDelegate ()
 
 @end
@@ -16,6 +16,8 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    OneViewController *oneViewController = [[OneViewController alloc] init];
+    self.window.rootViewController = oneViewController;
     // Override point for customization after application launch.
     return YES;
 }
---
+++ sdfsf/sdfsf/OneViewController.h
@@ -0,0 +1,17 @@
+//
+//  OneViewController.h
+//  sdfsf
+//
+//  Created by mac on 2019/1/10.
+//  Copyright © 2019 强淑婷. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface OneViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
